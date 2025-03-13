8a2e8d885515c071b32a930921f32da4da1eb819
Coryn <coryntries@gmail.com>
2025-03-14 00:47:53 +0800

    MVP

--- hello.js
+++ hello.js
@@ -13,6 +13,11 @@ window.button_function = function(n){
 
 
 //const button1 = document.getElementById('button_1_id');
+let mean_calibrate_dist = 0;
+let calibrate_dist = [];
+let dist = -1;
+let mode = 0; // 0: calibrate; 1: detect distance
+
 const h1 = document.getElementById('header_id');
 const detectButton = document.getElementById('detect_button_id');
 const runningMode = "IMAGE";
@@ -22,6 +27,8 @@ const faceImage = document.getElementById('photo_image_id');
 const imageDiv = document.getElementById('image_div_id');
 const video = document.getElementById('camera_preview_id');
 const captureButton = document.getElementById('capture_button_id');
+const instructionH1 = document.getElementById('mode_header_id');
+const verdictH1 = document.getElementById('verdict_header_id');
 
 
 navigator.mediaDevices.getUserMedia({video: true})
@@ -105,9 +112,17 @@ async function handleDetectClick(event){
     }    
 }
 
-function point(x, y, context_used, size){
+function get_dist(keypoint1, keypoint2){
+    let x1 = keypoint1.x;
+    let y1 = keypoint1.y;
+    let x2 = keypoint2.x;
+    let y2 = keypoint2.y;
+    return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+}
+
+function point(x, y, context_used, size, color="cyan"){
     // const context_used = canvas_used.getContext("2d");
-    context_used.fillStyle = "cyan";
+    context_used.fillStyle = color;
     context_used.fillRect(x-size/2,y-size/2,size,size);
 }
 
@@ -120,14 +135,49 @@ function displayImageDetections(detections, resultElement){
 
     for (let detection of detections.detections){
 
+        let colors = ["red", "green", "blue", "cyan", "#F0F8FF", "#7FFFD4"];
+        let color_counter = 0;
+
         for (let keypoint of detection.keypoints) {
 
             let x = canvas.width * keypoint.x;
             let y = canvas.height * keypoint.y;
-            point(x, y, context, 3);
+            point(x, y, context, 3, "#F0F8FF");
+            color_counter = color_counter +1;
 
         }
+        dist = get_dist(detection.keypoints[0], detection.keypoints[1]);
+        console.log("distance:", dist);
+
+        if (mode==0){
+            calibrate_dist.push(dist);
+            if (calibrate_dist.length>=3){
+                mode = 1;
+                instructionH1.innerHTML = "Calibration complete - press detect again to check if you are a safe distance away from your screen!";
+                mean_calibrate_dist = 0;
+                for (var i=0; i<calibrate_dist.length; i++){
+                    mean_calibrate_dist += calibrate_dist[i];
+                }
+                mean_calibrate_dist /= calibrate_dist.length;
+            }else{
+                instructionH1.innerHTML = `Calibration mode - please take ${3-calibrate_dist.length} more photo${3-calibrate_dist.length==1? "": "s"} of yourself at a safe distance from the screen!`
+            }
+        }else{
+            if (dist > mean_calibrate_dist * 1.2){
+                verdictH1.style.display='block';
+                verdictH1.innerHTML = "Don't be this close to the screen!";
+                verdictH1.className = 'verdict-red';
+            }else{
+                //good
+                verdictH1.style.display='block';
+                verdictH1.innerHTML = "You are a safe distance from your screen! Keep it up!";
+                verdictH1.className = 'verdict-green';
+            }
+        }
+
+
     }
+    // TODO: update number of pics, 
 }
 
 
